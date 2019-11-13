16c10e6bb67f81aca284f3eba7a92510a14f6be4
Avi Britman <aybritman@gmail.com>
2019-11-13 15:40:37 +0200

    .

--- exemple.js
+++ exemple.js
@@ -1,10 +1,42 @@
+const yemot_api = require("./");
 
 (async () => {
 
 	const y = new yemot_api();
-	
-	await y.connect("0773137770", "1234");
 
-	console.log(await y.exec("GetSession"));
+	await y.connect("0772221222", "659841");
+
+	let r;
+
+	/** קבלת מספר יחידות */
+	r = await y.exec("GetSession");
+
+	console.log(r);
+
+	/** העלאת קובץ */
+	let o = {
+
+		file: {
+			value: "12345",
+			options: {
+				filename: "123.txt",
+				contentType: "text/txt"
+			}
+		},
+		path: "ivr/123.txt"
+	};
+
+	r = await y.exec("UploadFile", o);
+
+	console.log(r);
+
+	/** הורדת קובץ */
+	o = {
+		path: "ivr/123.txt"
+	};
+
+	r = await y.exec("DownloadFile", o);
+
+	console.log(r);
 
 })();
